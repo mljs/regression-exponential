d56b4d284f93d7ffe5011e6ad79a23cf280f33d9
jobo322 <jose.bolanos@correounivalle.edu.co>
2024-05-02 23:31:21 -0500

    fix: improve type interface

--- regression-exponential.d.ts
+++ regression-exponential.d.ts
@@ -3,6 +3,8 @@ import BaseRegression from 'ml-regression-base';
 declare namespace ExponentialRegression {
   export interface ExponentialRegressionModel {
     name: 'exponentialRegression';
+    A: number;
+    B: number;
   }
 }
 
